9700ab70e39f8869fc30e2ac3b644e9842e915a5
Michiel Holtkamp <git@supermind.nl>
2013-07-03 23:47:38 +0200

    Fix unknown modes exception, code cleanup
    Also use length of prefix when calculating max nicklen

--- names.py
+++ names.py
@@ -19,6 +19,8 @@
 # Usage: /na
 #
 # History:
+# 2013-07-03, Michiel Holtkamp <weechat@elfstone.nl>:
+#     version 1.1: ignore 'unknown' modes, use prefix length, code cleanup
 # 2013-06-22, Michiel Holtkamp <weechat@elfstone.nl>:
 #     version 1.0: initial release
 #
@@ -35,66 +37,74 @@ def cmp_nick(a, b):
 
 	return cmp(a[1].lower(), b[1].lower())
 
+def _bold(s):
+	return "{}{}{}".format(weechat.color("bold"), s, weechat.color("reset"))
+
 def names_command_cb(data, buffer, args):
 	max_linelen = 80
 	buffer = weechat.current_buffer()
 	server = weechat.buffer_get_string(buffer, "localvar_server")
 	channel = weechat.buffer_get_string(buffer, "localvar_channel")
 	infolist = weechat.infolist_get("irc_nick", "", "%s,%s" % (server, channel))
-	if infolist:
-		nicks = []
-		maxlen = 0
-		modes = {'@': 0, '%': 0, '+': 0, ' ': 0}
-		while weechat.infolist_next(infolist):
-			nick = weechat.infolist_string(infolist, "name")
-			prefix = weechat.infolist_string(infolist, "prefix")
-			nicks.append((prefix, nick))
-			maxlen = max(maxlen, len(nick))
+	if not infolist:
+		return weechat.WEECHAT_RC_OK
+
+	timefmt = weechat.config_string(
+				weechat.config_get("weechat.look.buffer_time_format"))
+	formatted = "{} ".format(time.strftime(timefmt))
+	list_prefix = " " * len(formatted)
+
+	nicks = []
+	maxlen = 0
+	modes = {'@': 0, '%': 0, '+': 0, ' ': 0}
+	while weechat.infolist_next(infolist):
+		nick = weechat.infolist_string(infolist, "name")
+		prefix = weechat.infolist_string(infolist, "prefix")
+		nicks.append((prefix, nick))
+		maxlen = max(maxlen, len(nick))
+		try:
 			modes[prefix] += 1
-		weechat.infolist_free(infolist)
+		except KeyError:
+			pass # unknown prefix, it's ok because we don't show it
+	weechat.infolist_free(infolist)
 
-		nicks.sort(cmp_nick)
-		nicks_per_line = max_linelen // (maxlen + 4) # +prefix +[] +space
-		rows = int(math.ceil(float(len(nicks)) / nicks_per_line))
+	nicks.sort(cmp_nick)
+	# +4 = +prefix +[] +space
+	nicks_per_line = (max_linelen - len(list_prefix)) // (maxlen + 4)
+	rows = int(math.ceil(float(len(nicks)) / nicks_per_line))
+	fmt = "{}[{}{}{}{}{: <%d}{}]{} " % maxlen
 
-		prefix = weechat.prefix("network")
-		bracket = weechat.color("black")
-		title = weechat.color("green")
-		bold = weechat.color("bold")
-		reset = weechat.color("reset")
+	prefix = weechat.prefix("network")
+	bracket = weechat.color("black")
+	title = weechat.color("green")
+	bold = weechat.color("bold")
+	reset = weechat.color("reset")
 
-		fmt = "{}[{}{}{}{}{: <%d}{}]{} " % maxlen
-		timefmt = weechat.config_string(
-					weechat.config_get("weechat.look.buffer_time_format"))
-		formatted = "{} ".format(time.strftime(timefmt))
-		list_prefix = " " * len(formatted)
+	# now that we have all information, print it
+	weechat.prnt(buffer, "{}{}[{}Users {}{}{}]{}".format(
+			prefix, bracket, title, bold, channel, bracket, reset))
 
-		# now that we have all information, print it
-		weechat.prnt(buffer, "{}{}[{}Users {}{}{}]{}".format(
-				prefix, bracket, title, bold, channel, bracket, reset))
-
-		for row in xrange(rows):
-			line = "\t\t%s" % list_prefix
-			for col in xrange(nicks_per_line):
-				try:
-					nick = nicks[col * rows + row]
-				except IndexError:
-					break
-				line += fmt.format(bracket, reset, bold, nick[0], reset,
-									nick[1], bracket, reset)
-			weechat.prnt(buffer, line)
-		
-		weechat.prnt(buffer,
-			"{}{}{}{}: {}{}{} nicks ({}{}{} ops, {}{}{} halfops, {}{}{} voices, {}{}{} normals)".format(
-				prefix, bold, channel, reset, bold, len(nicks), reset,
-				bold, modes['@'], reset, bold, modes['%'], reset,
-				bold, modes['+'], reset, bold, modes[' '], reset
-			)
+	for row in xrange(rows):
+		line = "\t\t%s" % list_prefix
+		for col in xrange(nicks_per_line):
+			try:
+				nick = nicks[col * rows + row]
+			except IndexError:
+				break
+			line += fmt.format(bracket, reset, bold, nick[0], reset,
+								nick[1], bracket, reset)
+		weechat.prnt(buffer, line)
+	
+	weechat.prnt(buffer,
+		"{}{}: {} nicks ({} ops, {} halfops, {} voices, {} normals)".format(
+			prefix, _bold(channel), _bold(len(nicks)), _bold(modes['@']),
+			_bold(modes['%']), _bold(modes['+']), _bold(modes[' ']),
 		)
+	)
 
 	return weechat.WEECHAT_RC_OK
 
-weechat.register("names", "Michiel Holtkamp", "1.0", "GPL3", "Names script", "", "")
+weechat.register("names", "Michiel Holtkamp", "1.1", "GPL3", "Names script", "", "")
 weechat.hook_command("na", "Show nicks in columns instead of a list",
 	"",
 	"",
